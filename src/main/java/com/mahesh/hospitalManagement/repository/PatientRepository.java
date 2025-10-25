edeb987ad3c73288333c8aea3a74ea6e2e2afe37
Mahesh Warpe <maheshvarpe930@gmail.com>
2025-10-25 19:28:09 +0530

    Revert "create patient entity"
    This reverts commit 595881fd476b81963975b262ef579ece455dc779.

--- src/main/java/com/mahesh/hospitalManagement/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mahesh.hospitalManagement.repository;
-
-
-import com.mahesh.hospitalManagement.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-
-}
