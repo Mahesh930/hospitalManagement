fcb7cb89bec772179009200075bb0c865430d2e1
Mahesh Warpe <maheshvarpe930@gmail.com>
2025-11-06 08:58:09 +0530

    Add all Controllers

---
+++ src/main/java/com/mahesh/hospitalManagement/controller/PatientController.java
@@ -0,0 +1,26 @@
+package com.mahesh.hospitalManagement.controller;
+
+import com.mahesh.hospitalManagement.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/appointments")
+    public ResponseEntity<AppointmentResponseDto> createNewAppointment(@RequestBody CreateAppointmentRequestDto createAppointmentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.createNewAppointment(createAppointmentRequestDto));
+    }
+
+    @GetMapping("/profile")
+    private ResponseEntity<PatientResponseDto> getPatientProfile() {
+        Long patientId = 4L;
+        return ResponseEntity.ok(patientService.getPatientById(patientId));
+    }
+}
