52bc5342ee3e3814862616051a3726be38db3f19
Mahesh Warpe <maheshvarpe930@gmail.com>
2026-01-15 12:51:52 +0530

    Add oAuth2 authentication with different auth providers

--- src/main/java/com/mahesh/hospitalManagement/entity/User.java
+++ src/main/java/com/mahesh/hospitalManagement/entity/User.java
@@ -3,17 +3,15 @@ package com.mahesh.hospitalManagement.entity;
 
 import com.mahesh.hospitalManagement.entity.type.AuthProviderType;
 import com.mahesh.hospitalManagement.entity.type.RoleType;
+import com.mahesh.hospitalManagement.security.RolePermissionMapping;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @Entity
--- src/main/java/com/mahesh/hospitalManagement/security/AuthService.java
+++ src/main/java/com/mahesh/hospitalManagement/security/AuthService.java
@@ -5,12 +5,17 @@ import com.mahesh.hospitalManagement.dto.LoginResponseDto;
 import com.mahesh.hospitalManagement.dto.SignUpRequestDto;
 import com.mahesh.hospitalManagement.dto.SignupResponseDto;
 import com.mahesh.hospitalManagement.entity.User;
+import com.mahesh.hospitalManagement.entity.type.AuthProviderType;
 import com.mahesh.hospitalManagement.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,22 +37,61 @@ public class AuthService {
         String token = authUtil.generateAccessToken(user);
 
         return new LoginResponseDto(token,user.getId());
-
-
     }
 
-    public SignupResponseDto signup(SignUpRequestDto signupRequestDto) {
+    public User signUpInternal(SignUpRequestDto signupRequestDto, AuthProviderType authProviderType, String providerId){
         User user = userRepository.findByUsername(signupRequestDto.getUsername()).orElse(null);
 
         if(user != null ) throw new IllegalArgumentException("User is already present with this username ");
 
-        user = userRepository.save(User.builder()
-                .username(signupRequestDto.getUsername())
-                .password(passwordEncoder.encode(signupRequestDto.getPassword()))
-                .build()
-        );
+       user = User.builder()
+               .username(signupRequestDto.getUsername())
+               .providerId(providerId)
+               .providerType(authProviderType)
+               .build();
+       if (authProviderType == AuthProviderType.EMAIL){
+           user.setPassword(passwordEncoder.encode(signupRequestDto.getPassword()));
+       }
+
+       return userRepository.save(user);
+    }
+
+    public SignupResponseDto signup(SignUpRequestDto signupRequestDto) {
+        User user = signUpInternal(signupRequestDto, AuthProviderType.EMAIL, null);
 
         return new SignupResponseDto(user.getId(), user.getUsername());
+    }
 
+    @Transactional
+    public ResponseEntity<LoginResponseDto> handleOAuth2LoginRequest(OAuth2User oAuth2User, String registrationId) {
+        // fetch providerType and providerId
+        AuthProviderType authProviderType = authUtil.getProviderTypeFromRegistrationId(registrationId);
+        String providerId = authUtil.determineProviderIdFromOAuth2User(oAuth2User, registrationId);
+
+//        save the providerType and providerId info with user
+        User user = userRepository.findByProviderIdAndProviderType( providerId,authProviderType).orElse(null);
+
+        String email = oAuth2User.getAttribute("email");
+
+        User emailUser = userRepository.findByUsername(email).orElse(null);
+
+        if (user == null && emailUser == null){
+//        signup
+            String username = authUtil.determineUsernameFromOAuth2User(oAuth2User, registrationId, providerId);
+            user = signUpInternal( new LoginRequestDto(username, null), authProviderType,providerId);
+
+        }else if(user != null){
+            if(email != null && !email.isBlank() && !email.equals(user.getUsername()) ){
+                user.setUsername(email);
+                userRepository.save(user);
+            }
+        }else {
+            throw new BadCredentialsException("This email is already registered with provider "+ emailUser.getProviderType());
+        }
+//        if the user has an account: directly login
+//        otherwise, first signup and then login
+        LoginResponseDto loginResponseDto = new LoginResponseDto(authUtil.generateAccessToken(user), user.getId());
+
+        return ResponseEntity.ok(loginResponseDto);
     }
 }
